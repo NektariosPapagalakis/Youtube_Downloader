3dcc90c17decdd78f68dd13df33186db16fb5cf2
Nektatios Papagalakis <nektarios00007@gmail.com>
2022-05-09 15:33:12 +0300

    jjj

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter.simpledialog import askstring
 import youtube_dl
 from pytube import YouTube
 
@@ -17,21 +18,53 @@ def download_as_mp3(video_url):
     print("Download complete... {}".format(filename))
 
 
-# https://stackoverflow.com/questions/58662397/python-how-do-i-get-the-download-percentage-from-youtube-dl-when-im-downloading
+def get_video_name(url):
+    yt = YouTube(url)
+    return yt.title
+
 
 BACKGROUND_COLOR = "#303030"
 COLOR_1 = "#3796F6"
 COLOR_2 = "#626262"
 
-class YoutubeDownloader(Tk):
 
+class ChangeName(Tk):
+    def __init__(self, url):
+        super(ChangeName, self).__init__()
+        self.title('Change Name')
+        self.configure(background=BACKGROUND_COLOR)
+
+        label_url = Label(self, text=url, bg=BACKGROUND_COLOR, foreground=COLOR_1, font=("Arial", 10))
+        label_url.pack(pady=10,padx=50)
+
+        entry_name = Entry(self,width=50, background=COLOR_2, foreground=COLOR_1, font=("Arial", 12))
+        entry_name.delete(0, END)
+        entry_name.insert(0,get_video_name(url))
+        entry_name.pack(pady=(0,10),padx=50)
+
+        frame_buttons = Frame(self, background=BACKGROUND_COLOR)
+        frame_buttons.pack()
+        # Set
+        self.button_set_name = Button(frame_buttons, text="Set", bg=COLOR_2, foreground=COLOR_1,
+                                            font=("Arial", 15), width=12, cursor="hand2")
+        self.button_set_name.grid(row=0, column=0, pady=(0, 10),padx=10)
+        # ADD
+        self.button_cansel = Button(frame_buttons, text="Cansel", bg=COLOR_2, foreground=COLOR_1,
+                                      font=("Arial", 15), width=12,command=self.on_closing, cursor="hand2")
+        self.button_cansel.grid(row=0, column=1, pady=(0, 10),padx=10)
+
+    def on_closing(self):
+        self.destroy()
+
+
+class YoutubeDownloader(Tk):
     def __init__(self):
         super(YoutubeDownloader, self).__init__()
         self.song_list_url = []
         self.song_list_name = []
         self.mode = "single_song"
 
-        self.geometry("700x200")
+        self.geometry("700x250")
         self.title('Youtube Downloader')
         self.configure(background=BACKGROUND_COLOR)
         # Mode
@@ -61,7 +94,7 @@ class YoutubeDownloader(Tk):
         # Download
         self.button_download_video = Button(frame_buttons, text="Download", bg=COLOR_2, foreground=COLOR_1,
                                             command=self.download, font=("Arial", 17), width=15, cursor="hand2")
-        self.button_download_video.grid(row=0, column=0, padx=10)
+        self.button_download_video.grid(row=0, column=0, pady=(0, 10))
         # ADD
         self.button_add_song = Button(frame_buttons, text="Add", bg=COLOR_2, foreground=COLOR_1,
                                       font=("Arial", 17), width=15, command=self.add, cursor="hand2")
@@ -69,11 +102,10 @@ class YoutubeDownloader(Tk):
         self.button_clear_song = Button(frame_buttons, text="Clear", bg=COLOR_2, foreground=COLOR_1,
                                         font=("Arial", 17), width=15, command=self.clear, cursor="hand2")
         # Show Section
-        self.label_show = Label(self, text="", bg=BACKGROUND_COLOR, foreground=COLOR_1, font=("Arial", 10),
-                                justify=LEFT)
-        self.label_show.pack(pady=(15,5))
+        self.label_show_song_list = Label(self, text="", bg=BACKGROUND_COLOR, foreground=COLOR_1, font=("Arial", 10),
+                                          justify=LEFT)
 
-        #Bing Enter button
+        # Bing Enter button
         self.bind('<Return>', self.call_def_with_enter)
 
     def check_url(self):
@@ -114,39 +146,35 @@ class YoutubeDownloader(Tk):
         if self.check_url():
             video_url = self.entry_insert_url.get()
             self.song_list_url.append(video_url)
-
-            yt = YouTube(video_url)
-            self.song_list_name.append(yt.title)
+            self.song_list_name.append(get_video_name(video_url))
             text = ""
             for i in range(len(self.song_list_url)):
                 text = text + str(i) + "  :  " + self.song_list_name[i] + "\n"
                 text = text + "       " + self.song_list_url[i] + "\n" + "\n"
-            self.label_show.config(text=text)
-            self.geometry("700x"+str(200+(50*len(self.song_list_url))))
+            self.label_show_song_list.config(text=text)
+            self.geometry("700x" + str(250 + (50 * len(self.song_list_url))))
 
     def clear(self):
         self.song_list_url = []
         self.song_list_name = []
-        self.label_show.config(text="")
+        self.label_show_song_list.config(text="")
+        self.geometry("700x250")
 
     def switch_mode(self):
         if self.mode == "single_song":
             self.button_mode.config(text="Mode Song List : ON", font=("Arial", 12, "underline"))
             self.mode = "song_list"
-            self.button_add_song.grid(row=0, column=1, padx=10)
-            self.button_clear_song.grid(row=0, column=2, padx=10)
+            self.button_add_song.grid(row=1, column=0, padx=10)
+            self.button_clear_song.grid(row=1, column=1, padx=10)
+            self.label_show_song_list.pack(pady=(15, 5))
         else:
             self.button_mode.config(text="Mode Song List : OFF", font=("Arial", 12))
             self.mode = "single_song"
             self.button_add_song.grid_forget()
             self.button_clear_song.grid_forget()
+            self.label_show_song_list.pack_forget()
 
-    def switch_mode_to_song_list(self):
-        self.mode = "song_list"
-        self.button_mode_song_list.config(font=("Arial", 12, "underline"))
-        self.button_mode_singe_song.config(font=("Arial", 12))
-
-    def call_def_with_enter(self,e):
+    def call_def_with_enter(self, e):
         if self.mode == "single_song":
             self.download()
         else:
@@ -154,6 +182,10 @@ class YoutubeDownloader(Tk):
 
     def clear_input(self, e):
         self.entry_insert_url.delete(0, END)
+
+    def change_name(self):
+        if self.check_url():
+            ChangeName(self.entry_insert_url.get())
 
     def on_closing(self):
         self.destroy()
