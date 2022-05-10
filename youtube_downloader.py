beb182ad98b6c30ea5600d8c98c61340bbc72fa3
Nektatios Papagalakis <nektarios00007@gmail.com>
2022-05-11 00:09:31 +0300

    fff

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -23,6 +23,19 @@ def get_video_name(url):
     return yt.title
 
 
+def check_url(url):
+    checks = True
+    if url == "":
+        messagebox.showerror("Error", "You must provide a Youtube url")
+        checks = False
+    elif not (url.startswith('https://www.youtube.com/watch')):
+        messagebox.showerror("Error", "Improper url")
+        checks = False
+    if checks:
+        return True
+    return False
+
+
 BACKGROUND_COLOR = "#303030"
 COLOR_1 = "#3796F6"
 COLOR_2 = "#626262"
@@ -33,7 +46,9 @@ class YoutubeDownloader(Tk):
         super(YoutubeDownloader, self).__init__()
         self.song_list_url = []
         self.song_list_name = []
+        self.song_list_labels = []
         self.mode = "single_song"
+        self.count_of_songs = 0
 
         self.geometry("700x250")
         self.title('Youtube Downloader')
@@ -61,7 +76,7 @@ class YoutubeDownloader(Tk):
 
         # Buttons
         frame_buttons = Frame(self, background=BACKGROUND_COLOR)
-        frame_buttons.pack()
+        frame_buttons.pack(pady=(0, 15))
         # Download
         self.button_download_video = Button(frame_buttons, text="Download", bg=COLOR_2, foreground=COLOR_1,
                                             command=self.download, font=("Arial", 17), width=15, cursor="hand2")
@@ -72,31 +87,27 @@ class YoutubeDownloader(Tk):
         # Clear
         self.button_clear_song = Button(frame_buttons, text="Clear", bg=COLOR_2, foreground=COLOR_1,
                                         font=("Arial", 17), width=15, command=self.clear, cursor="hand2")
-        # Show Section
-        self.label_show_song_list = Label(self, text="", bg=BACKGROUND_COLOR, foreground=COLOR_1, font=("Arial", 10),
-                                          justify=LEFT)
-
         # Bing Enter button
         self.bind('<Return>', self.call_def_with_enter)
 
-    def check_url(self):
-        checks = True
-        url = self.entry_insert_url.get()
-        if url == "":
-            messagebox.showerror("Error", "You must provide a Youtube url")
-            checks = False
-        elif not (url.startswith('https://www.youtube.com/watch')):
-            messagebox.showerror("Error", "Improper url")
-            checks = False
-        if checks:
-            return True
-        return False
+        self.label_progress = Label(self, text="", bg=BACKGROUND_COLOR, foreground=COLOR_1,
+                                 font=("Arial", 17))
+        self.label_progress.pack()
+
+    def add_song_label(self, text):
+        label = Label(self, text=text, bg=BACKGROUND_COLOR, foreground=COLOR_1, font=("Arial", 10),
+                      justify=LEFT)
+        label.pack(pady=5, anchor="w", padx=(50, 0))
+        self.song_list_labels.append(label)
+        number = len(self.song_list_labels) - 1
+        label.bind("<Button-1>", lambda event: self.remove_url(number))
+        self.geometry("700x" + str(250 + (50 * self.count_of_songs)))
 
     def download(self):
         if self.mode == "single_song":
-            if self.check_url():
+            url = self.entry_insert_url.get()
+            if check_url(url):
                 error = False
-                url = self.entry_insert_url.get()
                 try:
                     download_as_mp3(url, get_video_name(url))
                     messagebox.showinfo('Complete', 'Download is Complete')
@@ -111,33 +122,35 @@ class YoutubeDownloader(Tk):
         else:
             error = False
             for url in self.song_list_url:
-                try:
-                    download_as_mp3(url, get_video_name(url))
-                except youtube_dl.utils.DownloadError:
-                    messagebox.showerror("Error", "You must provide a url from Youtube")
-                    error = True
-                except:
-                    error = True
-                    messagebox.showerror("Error", "Something went wrong !")
+                if url != "x":
+                    if check_url(url):
+                        try:
+                            download_as_mp3(url, get_video_name(url))
+                        except youtube_dl.utils.DownloadError:
+                            messagebox.showerror("Error", "You must provide a url from Youtube")
+                            error = True
+                        except:
+                            error = True
+                            messagebox.showerror("Error", "Something went wrong !")
+                        self.label_progress.config(text=self.label_progress.cget("text")+"1")
             if not error:
                 messagebox.showinfo('Complete', 'Download is Complete')
 
     def add(self):
-        if self.check_url():
-            video_url = self.entry_insert_url.get()
+        video_url = self.entry_insert_url.get()
+        if check_url(video_url):
+            self.count_of_songs = self.count_of_songs + 1
             self.song_list_url.append(video_url)
             self.song_list_name.append(get_video_name(video_url))
-            text = ""
-            for i in range(len(self.song_list_url)):
-                text = text + str(i) + "  :  " + self.song_list_name[i] + "\n"
-                text = text + "       " + self.song_list_url[i] + "\n" + "\n"
-            self.label_show_song_list.config(text=text)
-            self.geometry("700x" + str(250 + (50 * len(self.song_list_url))))
+            text = str(self.count_of_songs) + "  :  " + get_video_name(video_url) + "\n" + "       " + video_url
+            self.add_song_label(text)
 
     def clear(self):
         self.song_list_url = []
         self.song_list_name = []
-        self.label_show_song_list.config(text="")
+        for label in self.song_list_labels:
+            label.pack_forget()
+        self.song_list_labels = []
         self.geometry("700x250")
 
     def switch_mode(self):
@@ -146,7 +159,7 @@ class YoutubeDownloader(Tk):
             self.mode = "song_list"
             self.button_add_song.grid(row=1, column=0, padx=10)
             self.button_clear_song.grid(row=1, column=1, padx=10)
-            self.label_show_song_list.pack(pady=(15, 5))
+            # self.label_show_song_list.pack(pady=(15, 5))
         else:
             self.button_mode.config(text="Mode Song List : OFF", font=("Arial", 12))
             self.mode = "single_song"
@@ -162,6 +175,18 @@ class YoutubeDownloader(Tk):
 
     def clear_input(self, e):
         self.entry_insert_url.delete(0, END)
+
+    def remove_url(self, number_of_url):
+        number_of_url = int(number_of_url)
+        print(number_of_url)
+        if len(self.song_list_labels) > 0:
+            self.count_of_songs = self.count_of_songs - 1
+            self.song_list_labels[number_of_url].pack_forget()
+            self.geometry("700x" + str(250 + (50 * self.count_of_songs)))
+            self.song_list_url[number_of_url] = "x"
+            self.song_list_name[number_of_url] = "x"
+            for x in self.song_list_name:
+                print(x)
 
     def on_closing(self):
         self.destroy()
